5009e412fa1c4987d501210d72863f97ae0420e3
Rafsun Masud <rafsun82@gmail.com>
2021-12-23 15:10:49 -0500

    Update auth subcommands

--- onm/cli/auth.py
+++ onm/cli/auth.py
@@ -2,34 +2,57 @@ from os import terminal_size
 import click
 from onm.clients import tools
 
-@click.command()
+@click.group()
 @click.pass_context
-@click.option('-p', '--print', 'prnt', default=False, help='Prints the logged in user info.', is_flag=True)
-def auth(ctx, prnt):
-    # --print
-    if prnt:
-        if ctx.obj.msc.user is None:
-            click.echo("No user is logged in.")
-        else:
-            click.echo(f"Logged in as: {ctx.obj.msc.user['displayName']} ({ctx.obj.msc.user['userPrincipalName']})")     
-                   
-    # default (login)
+def auth(ctx):
+    pass 
+
+
+@auth.command(name="st", short_help="login status")
+@click.pass_context
+def status(ctx):
+    """Prints the current logged in user."""
+    if ctx.obj.msc.user is None:
+        click.echo("No user is logged in.")
     else:
-        # Fetch token
-        click.echo("Go to this URL, copy and paste the final redirected uri below.")
-        click.echo(f"\n{ctx.obj.msc.get_auth_url()}\n")
-        response_uri = click.prompt("Paste here")
-        code = ctx.obj.msc.get_code(response_uri)
+        click.echo(f"Logged in as: {ctx.obj.msc.user['displayName']} ({ctx.obj.msc.user['userPrincipalName']})")     
+                   
 
-        click.echo("Logging in...")
-        ctx.obj.msc.fetch_token(code)
+@auth.command(name="li", short_help="log in")
+@click.pass_context
+def login(ctx):
+    """Prompts login."""
 
-        # Fetch user
-        ctx.obj.msc.fetch_user()
+    # Check if already logged in
+    if ctx.obj.msc.user is not None and \
+        not click.confirm('Do you want to remove current user first, and then log in?'):
+            return
 
-        # Save user and token 
-        tools.save_dict(str(ctx.obj.config.USER_INFO_PATH), ctx.obj.msc.user)
-        tools.save_dict(str(ctx.obj.config.TOKEN_PATH), ctx.obj.msc.oauth.token)
+    # Fetch token
+    click.echo("Go to this URL, copy and paste the final redirected uri below.")
+    click.echo(f"\n{ctx.obj.msc.get_auth_url()}\n")
+    response_uri = click.prompt("Paste here")
+    code = ctx.obj.msc.get_code(response_uri)
 
-        # Display success info
-        click.echo(f"Logged in as: {ctx.obj.msc.user['displayName']} ({ctx.obj.msc.user['userPrincipalName']})")
+    click.echo("Logging in...")
+    ctx.obj.msc.fetch_token(code)
+
+    # Fetch user
+    ctx.obj.msc.fetch_user()
+
+    # Save user and token 
+    ctx.invoke(logout)
+    tools.save_dict(str(ctx.obj.config.USER_INFO_PATH), ctx.obj.msc.user)
+    tools.save_dict(str(ctx.obj.config.TOKEN_PATH), ctx.obj.msc.oauth.token)
+
+    # Display success info
+    click.echo(f"Logged in as: {ctx.obj.msc.user['displayName']} ({ctx.obj.msc.user['userPrincipalName']})")
+
+
+@auth.command(name="lo", short_help="log out")
+@click.pass_context
+def logout(ctx):
+    """Clears user information from local storage."""
+    ctx.obj.config.TOKEN_PATH.unlink(missing_ok=True)
+    ctx.obj.config.USER_INFO_PATH.unlink(missing_ok=True)
+    pass
